84a96da227fa48b11b1f87d39098cd5f6a68d7e8
Pablo Yucra <cesar.yucra12@gmail.com>
2019-08-16 12:47:39 -0500

    last change

--- 3DGames_Project_Boost/Assets/Rocket.cs
+++ 3DGames_Project_Boost/Assets/Rocket.cs
@@ -5,33 +5,56 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Trust();
+        Rotate();
     }
-
-    private void ProcessInput()
+    private void Trust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rcsThrust = 10f;
+        
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            float rotationSpeed = rcsThrust * Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false;
     }
+
+
 }
